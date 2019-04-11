72e9b75209ce6df89417402826e93aa9e165e9e3
Ehab Ibrahim <emibrahim@eng.zu.edu.eg>
2019-04-11 16:44:38 +0900

    BS2048 - MOM1 - acc34.59 - Merge bn.rv.sqrt() with bn.weight

--- DL/edit.sh
+++ DL/edit.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 QPs=(22)
-Dir=blowing
+Dir=VALend20
 
 for qp in "${QPs[@]}";
 do
-	cd ~/git-repos/HM16.9/DL/$Dir/$qp
-    List=(emb* lins*-weight.csv lins*-bias.csv bns*-weight.csv bns*-bias.csv bns*-running_mean.csv bns*-running_var.csv mapper*)
-	
-	for file in *;
-	do
+    cd ~/git-repos/HM16.9/DL/$Dir/$qp
+    List=(emb* lins*-weight.csv lins*-bias.csv bns*-rv-weight.csv bns*-bias.csv bns*-rm.csv mapper*)
+    
+    for file in *;
+    do
         truncate -s-2 $file
         sed -i -e 's/^/\t\t\t/' $file
         echo ";" >> "$file"
-	done
-	
-	for i in "${!List[@]}";
-	do
-		mv "${List[$i]}" "$((i+1)).${List[$i]}"
-	done
+    done
+    
+    for i in "${!List[@]}";
+    do
+        mv "${List[$i]}" "$((i+1)).${List[$i]}"
+    done
 done
